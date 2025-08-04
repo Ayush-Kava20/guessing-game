6bdde3da29f7a901f2cbce83ac5ed1a630b08d10
Advant ayush <ayush.kava@advant.xyz>
2025-08-04 11:29:16 +0530

    add the use of refs

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef( function ResultModal({targetTime, remainingTime, onReset}, ref) {
 
@@ -16,7 +17,7 @@ const ResultModal = forwardRef( function ResultModal({targetTime, remainingTime,
         }
     })
 
-  return (
+  const modalContent = (
     <dialog ref={dialog} className="result-modal">
         {userLost && <h2>You Lost</h2>}
         {!userLost && <h2>Your Score: {score}</h2>}
@@ -29,8 +30,11 @@ const ResultModal = forwardRef( function ResultModal({targetTime, remainingTime,
         <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>
+    </dialog> 
   )
+
+  return createPortal(modalContent, document.getElementById('modal'));
+  
 })
 
 export default ResultModal; 
